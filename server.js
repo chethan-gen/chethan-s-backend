e0faac79a30379c7903d731dd87f59e2c0e372d2
chethan-gen <chaithuregala123@gmailcom>
2025-03-06 14:54:53 +0530

    adding changes

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 app.get('/ping',(req,res)=>{
     res.send('pong')
 })
+
+app.get('/get',(req,res)=>{
+    res.send('get')
+})
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
     })
